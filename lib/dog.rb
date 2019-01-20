6b12fc42b38ce5634f7cb11dc7b45cbdb7ee7848
Johnathon Wood <jaywood128@gmail.com>
2019-01-20 22:35:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,7 +77,7 @@ class Dog
     if !dog.empty?
       dog_data = dog[0]
       dog = self.new(id: dog[0], name: dog[1], breed: dog[2])
-    else 
+    else
       dog = self.create(name: name, breed: breed)
     end
     dog
