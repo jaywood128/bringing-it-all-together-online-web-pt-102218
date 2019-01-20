e1683cbe9c6663b095497b5686269c466cd68c71
Johnathon Wood <jaywood128@gmail.com>
2019-01-20 22:55:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -85,7 +85,7 @@ class Dog
 
   def self.new_from_db(row)
     binding.pry
-    dog = self.new(id: row[0],name: row[1], breed:[2])
+    dog = self.new(id: row[0],name: row[1], breed:row[2])
 
   end
 end
